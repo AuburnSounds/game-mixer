fcc783c58275be2bc258320016db4818be973531
Guillaume Piolat <contact@auburnsounds.com>
2021-08-13 02:19:19 +0200

    Cleanup

--- examples/music-playback/source/main.d
+++ examples/music-playback/source/main.d
@@ -5,13 +5,13 @@ import gamemixer;
 
 void main()
 {
-    // The whole API is available through `IMixer` and the interfaces it may return.
-    IMixer mixer = mixerCreate(); // Create with defaults (48000Hz, 512 or 1024 samples of software latency).
+    IMixer mixer = mixerCreate();
 
     IAudioSource music = mixer.createSourceFromFile("lits.xm");
     mixer.play(music);
 
     writeln("Press ENTER to end the playback...");
     readln();
-    mixerDestroy(mixer); // this cleans up everything created through `mixer`.
+
+    mixerDestroy(mixer);
 }
