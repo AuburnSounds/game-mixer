3fe3db93e5a660078808aed70704aa40f48a6557
Guillaume Piolat <contact@auburnsounds.com>
2021-08-16 03:25:00 +0200

    Cleanup examples.

--- examples/music-playback/source/main.d
+++ examples/music-playback/source/main.d
@@ -5,23 +5,10 @@ import gamemixer;
 
 void main()
 {
-    // Decode 8x th same MP3 to send in 80 playback channels, in order to create problems.
-    MixerOptions options;
-    options.numChannels = 80;
     IMixer mixer = mixerCreate(options);
     
-    foreach(n; 0..10)
-    {
-        IAudioSource music = mixer.createSourceFromFile("Malicorne - Vive la lune.mp3");
-        mixer.play(music, 0.1f);
-        mixer.play(music, 0.1f);
-        mixer.play(music, 0.1f);
-        mixer.play(music, 0.1f);
-        mixer.play(music, 0.1f);
-        mixer.play(music, 0.1f);
-        mixer.play(music, 0.1f);
-        mixer.play(music, 0.1f);
-    }
+    IAudioSource music = mixer.createSourceFromFile("lits.xm");
+    mixer.play(music);
 
     writeln("Press ENTER to end the playback...");
     readln();
